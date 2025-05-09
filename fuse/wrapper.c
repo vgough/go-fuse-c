b2ec26b7106caa9c5095c8c1a6d1f7d5396ed842
Valient Gough <vgough@pobox.com>
2025-05-09 17:40:30 +0000

    format file

--- fuse/wrapper.c
+++ fuse/wrapper.c
@@ -4,8 +4,8 @@
 #include <osxfuse/fuse/fuse_common.h>  // for fuse_mount, etc
 #include <osxfuse/fuse/fuse_opt.h>     // for fuse_opt_free_args, etc
 #else
-#include <fuse_lowlevel.h>
 #include <fuse_common.h>
+#include <fuse_lowlevel.h>
 #endif
 
 #include <stdio.h>      // for NULL
@@ -63,64 +63,60 @@ static struct fuse_lowlevel_ops bridge_ll_ops = {
 
 int MountAndRun(int id, int argc, char *argv[]) {
   struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-	struct fuse_session *se;
-	struct fuse_cmdline_opts opts;
-	struct fuse_loop_config config;
+  struct fuse_session *se;
+  struct fuse_cmdline_opts opts;
+  struct fuse_loop_config config;
   int ret = -1;
 
-	if (fuse_parse_cmdline(&args, &opts) != 0) {
-		return 1;
+  if (fuse_parse_cmdline(&args, &opts) != 0) {
+    return 1;
   }
-	if (opts.show_help) {
-		printf("usage: %s [options] <mountpoint>\n\n", argv[0]);
-		fuse_cmdline_help();
-		fuse_lowlevel_help();
-		ret = 0;
-		goto err_out1;
-	} else if (opts.show_version) {
-		printf("FUSE library version %s\n", fuse_pkgversion());
-		fuse_lowlevel_version();
-		ret = 0;
-		goto err_out1;
-	}
-
-	if(opts.mountpoint == NULL) {
-		printf("usage: %s [options] <mountpoint>\n", argv[0]);
-		printf("       %s --help\n", argv[0]);
-		ret = 1;
-		goto err_out1;
-	}
-
-	se = fuse_session_new(&args, &bridge_ll_ops,
-			      sizeof(bridge_ll_ops), &id);
-	if (se == NULL)
-	    goto err_out1;
-
-	if (fuse_set_signal_handlers(se) != 0)
-	    goto err_out2;
-
-	if (fuse_session_mount(se, opts.mountpoint) != 0)
-	    goto err_out3;
-
-	fuse_daemonize(opts.foreground);
-
-	/* Block until ctrl+c or fusermount -u */
-	if (opts.singlethread)
-		ret = fuse_session_loop(se);
-	else {
-	  config.clone_fd = opts.clone_fd;
-		config.max_idle_threads = opts.max_idle_threads;
-		ret = fuse_session_loop_mt(se, &config);
+  if (opts.show_help) {
+    printf("usage: %s [options] <mountpoint>\n\n", argv[0]);
+    fuse_cmdline_help();
+    fuse_lowlevel_help();
+    ret = 0;
+    goto err_out1;
+  } else if (opts.show_version) {
+    printf("FUSE library version %s\n", fuse_pkgversion());
+    fuse_lowlevel_version();
+    ret = 0;
+    goto err_out1;
   }
 
-	fuse_session_unmount(se);
-  err_out3:
-    fuse_remove_signal_handlers(se);
-  err_out2:
-    fuse_session_destroy(se);
-  err_out1:
-    free(opts.mountpoint);
-    fuse_opt_free_args(&args);
+  if (opts.mountpoint == NULL) {
+    printf("usage: %s [options] <mountpoint>\n", argv[0]);
+    printf("       %s --help\n", argv[0]);
+    ret = 1;
+    goto err_out1;
+  }
+
+  se = fuse_session_new(&args, &bridge_ll_ops, sizeof(bridge_ll_ops), &id);
+  if (se == NULL) goto err_out1;
+
+  if (fuse_set_signal_handlers(se) != 0) goto err_out2;
+
+  if (fuse_session_mount(se, opts.mountpoint) != 0) goto err_out3;
+
+  fuse_daemonize(opts.foreground);
+
+  /* Block until ctrl+c or fusermount -u */
+  if (opts.singlethread)
+    ret = fuse_session_loop(se);
+  else {
+    config.clone_fd = opts.clone_fd;
+    config.max_idle_threads = opts.max_idle_threads;
+    ret = fuse_session_loop_mt(se, &config);
+  }
+
+  fuse_session_unmount(se);
+err_out3:
+  fuse_remove_signal_handlers(se);
+err_out2:
+  fuse_session_destroy(se);
+err_out1:
+  free(opts.mountpoint);
+  fuse_opt_free_args(&args);
 
   return ret ? 1 : 0;
 }
